837f44fd77710e30c27fe68c4ee81e8171284397
anujeditsbyanuj-bit <anujeditsbyanuj@gmail.com>
2026-09-11 06:42:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Telegram Bot Configuration
-BOT_TOKEN = "8380940816:AAF0pJrqrFVOCs_0n2lQMynyeXlY788ennA"
-OWNER_ID = 1357978966
-API_ID = "20239261"
-API_HASH = "af61dab399ed3220a3a81570b56bd29d"
+BOT_TOKEN = "8986691330:AAHqk29Id7Rxz_7ErbJeli4uhnOMPrUcH0g"
+OWNER_ID = 8931907813
+API_ID = "33029767"
+API_HASH = "5d897bed11bc8b062a12f6c1c3c5360a"
 
 # Logging Configuration
-LOG_CHANNEL_ID = None  # Example: -100123456789. Set to None to disable.
+LOG_CHANNEL_ID = -1003925649805  # Example: -100123456789. Set to None to disable.
 
 # Database Configuration
 DB_URL = "mongodb+srv://tw4all:itzmejd%40143@cluster0.vmw2oby.mongodb.net/?appName=Cluster0"
